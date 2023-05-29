c999efc9a9b378fa54caede8e7b571c1589d18f3
WonGi-Kim <kwg970310@gmail.com>
2023-05-30 01:51:30 +0900

    Initial Commit

---
+++ LoA To-DO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoA To-DO
+//
+//  Created by 김원기 on 2023/05/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
